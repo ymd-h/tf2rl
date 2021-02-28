fa4fc8c00ac991810c6d6171e7070ef266275f94
Yamada <ymd_h@outlook.jp>
2021-02-28 14:56:45 +0900

    Add explorer

--- tf2rl/algos/impala.py
+++ tf2rl/algos/impala.py
@@ -2,7 +2,7 @@ import time
 import logging
 import multiprocessing
 
-from cpprb import ReplayBuffer, MPPrioritizedReplayBuffer
+from cpprb import ReplayBuffer, MPReplayBuffer
 
 
 def import_tf():
@@ -15,8 +15,52 @@ def import_tf():
 
 
 
-def explorer():
-    pass
+def explorer(global_rb, queue, trained_steps, is_training_done,
+             env_fn, policy_fn, set_weights_fn, nsteps=100):
+    """
+
+    """
+    import_tf()
+    logger = logger.getLogger("tf2rl")
+
+    env = env_fn()
+
+    policy = policy_fn(env=env, name="Explorer")
+    kwargs = get_default_rb_dict(nsteps)
+    kwargs["env_dict"]["logp"] = {}
+    local_rb = ReplayBuffer(**kwargs)
+    local_idx = np.arange(local_rb.get_buffer_size(), dtype=np.int)
+
+    obs = env.reset()
+    while not is_trainig_don.is_set():
+        if not queue.empty():
+            w = queue.get()
+            set_weights_fn(policy_fn,w)
+
+        for _ in range(nsteps):
+            action, logp, v = policy.get_action_and_val(obs)
+            next_obs, rew, done, _ = env.step(action)
+
+            local_rb.add(obs=obs,
+                         act=action,
+                         rew=rew,
+                         next_obs=next_obs,
+                         done=done,
+                         logp=logp)
+
+            if done:
+                local_rb.on_episode_end()
+                obs = env.reset()
+                break
+            else:
+                obs = next_obs
+
+
+        rollout = local_rb.get_all_transitions()
+        rollout["LSTM"] = LSTM
+        global_rb.add(**rollout)
+        local_rb.clear()
+
 
 def learner():
     pass
